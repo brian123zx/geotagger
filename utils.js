85a6b2f4c0cc88256c8b6941cd5052d3770d0b49
Brian Cooper <brianzx@gmail.com>
2018-10-22 22:07:02 -0700

    add makeFullString util function

--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
+import { map } from 'lodash';
 const geolib = require('geolib');
 const parseLatLon = (lat, lon) => {
+	// TODO: refactor similar to makeFullString
 	var deg, min, sec;
 
 	var latitude = decimalToSexagesimal(lat);
@@ -10,9 +12,14 @@ const parseLatLon = (lat, lon) => {
 		GPSLongitude: `${longitude.deg} deg ${longitude.min}' ${Math.floor(longitude.sec*100)/100}" ${lon < 0 ? 'W' : 'E'}`,
 		GPSLatitudeRef: lat < 0 ? 'South' : 'North',
 		GPSLongitudeRef: lon < 0 ? 'West' : 'East',
-		fullString: `${latitude.deg}° ${latitude.min}' ${Math.floor(latitude.sec*100)/100}" ${lat < 0 ? 'S' : 'N'} ${longitude.deg}° ${longitude.min}' ${Math.floor(longitude.sec*100)/100}" ${lon < 0 ? 'W' : 'E'}`,
 	};
 };
+
+const makeFullString = (coords) => {
+	const mappedCoords = map(coords, decimalToSexagesimal);
+	return `${mappedCoords.latitude.deg}° ${mappedCoords.latitude.min}' ${Math.floor(mappedCoords.latitude.sec*100)/100}" ${coords.latitude < 0 ? 'S' : 'N'} ${mappedCoords.longitude.deg}° ${mappedCoords.longitude.min}' ${Math.floor(mappedCoords.longitude.sec*100)/100}" ${coords.longitude < 0 ? 'W' : 'E'}`;
+};
+
 const decimalToSexagesimal = (dec) => {
 	var deg, min, sec;
 	const sexagesimalRegex = /^(\d{1,3})°\ (\d{1,3})'\ ([\d.]+)"$/;
@@ -23,6 +30,7 @@ const decimalToSexagesimal = (dec) => {
 		sec
 	};
 };
+
 module.exports = {
 	parseLatLon,
 	decimalToSexagesimal,
